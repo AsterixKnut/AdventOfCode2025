caed6d44426cc9e3d2e792bf612bc1da1cb11753
Asterix_Knut <knut.visnes@gmail.com>
2025-12-01 11:51:55 +0100

    Setup: Implement main function to run each day's code

---
+++ src/days/day1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace day01 {
+
+void solve() {
+    std::cout << "Hello day 1!" << std::endl;
+}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <functional>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+namespace day01 { void solve(); }
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: ./aoc <day>\n";
+        return 1;
+    }
+
+    int day = std::stoi(argv[1]);
+
+    // Solver table
+    std::vector<std::function<void()>> solvers = {
+        nullptr, // index 0 unsused
+        day01::solve
+    };
+
+    // Warn if selected day is not implemented:
+    if (day <= 0 || day >= solvers.size() || !solvers[day]) {
+        std::cerr << "Day " << day << " is not implemented\n";
+        return 1;
+    }
+
+    // Solve
+    solvers[day]();
+
     return 0;
 }
