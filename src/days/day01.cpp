c22900539daa6e4a393ecd733ea89d2e1b7b220d
Asterix_Knut <knut.visnes@gmail.com>
2025-12-01 13:53:13 +0100

    Day1: Implement solution for part 2

--- src/days/day01.cpp
+++ src/days/day01.cpp
@@ -8,6 +8,8 @@ void solve() {
     // Get lines from input file
     auto lines = readLines(std::string(PROJECT_DIR) + "/input/day01.txt");
 
+    // ============================= PART 1 =========================================
+
     // Variables to find solution:
     int count = 0;
     int dial = 50;
@@ -27,14 +29,38 @@ void solve() {
         dial += num;
 
         // Make sure the dial points to a legal number:
-        while (dial> highestNum) { dial -= totalNumbers; }
-        while (dial < 0) { dial += totalNumbers; }
+        while (dial > highestNum) dial -= totalNumbers;
+        while (dial < 0) dial += totalNumbers;
 
         // Update count if we are at 0:
         if (dial == 0) count++;
     }
 
     std::cout << "Day 1, part 1: " << count << std::endl;
+
+    // ============================ PART 2 =============================================
+    count = 0;
+    dial = 50;
+
+    // Loop thorugh each line
+    for (const auto& line : lines) {
+        const char dir = line[0];
+        int num = std::stoi(line.substr(1));
+
+        int rotateDirection = 1;
+        if (dir == 'L') rotateDirection = -1;
+
+        for (int i = 0; i < std::abs(num); i++) {
+            dial += rotateDirection;
+            
+            if (dial < lowestNum) dial += totalNumbers;
+            else if (dial > highestNum) dial -= totalNumbers;
+
+            if (dial == 0) count++;
+        }
+    }
+
+    std::cout << "Day 1, part 2: " << count << std::endl;
 }
 
 }
