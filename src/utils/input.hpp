7832c15980e4616a3005395e1fd96c99e3eedb05
Asterix_Knut <knut.visnes@gmail.com>
2025-12-02 12:06:37 +0100

    Utils: Add function for splitting strings

--- src/utils/input.hpp
+++ src/utils/input.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <ios>
+#include <sstream>
 
 inline std::vector<std::string> readLines(const std::string& filepath) {
     std::vector<std::string> lines{};
@@ -24,4 +25,17 @@ inline std::vector<std::string> readLines(const std::string& filepath) {
     readFile.close();
 
     return lines;
+}
+
+inline std::vector<std::string> splitString(const std::string& string, const char delim) {
+    std::vector<std::string> splits{};
+    std::string split;
+
+    std::stringstream ss(string);
+
+    while (std::getline(ss, split, delim)) {
+        splits.push_back(split);
+    }
+
+    return splits;
 }
