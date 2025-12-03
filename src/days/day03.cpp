f08e95122f4ec83d18262ef22e88390d6deded4c
Asterix_Knut <knut.visnes@gmail.com>
2025-12-03 18:37:59 +0100

    Day03: Implement solution for part 2

--- src/days/day03.cpp
+++ src/days/day03.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <numeric>
+#include <cmath>
+#include <algorithm>
 
 #include "utils/input.hpp"
 
@@ -25,11 +27,47 @@ public:
     char prevHighest_;
 };
 
+unsigned long long getFirstMax(std::string& string, int numBatteries, std::vector<char>& maxValues) {
+    unsigned long long sum = 0ull;
+    unsigned long long power = 1;
+
+    int lastIndex = string.size() - 1;
+
+    for (int i = 0; i < numBatteries; i++) {
+        sum += (string[lastIndex - i]-'0') * power;
+        maxValues.push_back(string[lastIndex - i]); // Add current max values
+
+        power *= 10;
+    }
+
+    std::reverse(maxValues.begin(), maxValues.end());
+
+    return sum;
+}
+
+unsigned long long calculateSum(char newValue, std::vector<char>& values) {
+    unsigned long long sum = 0;
+    unsigned long long power = 1;
+
+    int lastIndex = values.size() - 1;
+
+    for (int i = lastIndex; i >= 0; i--) {
+        sum += (values[i] - '0') * power;
+        power *= 10;
+    }
+
+    sum += (newValue - '0') * power;
+
+    return sum;
+}
+
 void solve() {
     auto lines = readLines(std::string(PROJECT_DIR) + "/input/day03.txt");
 
-    std::vector<int> joltages{};
+    std::vector<unsigned long long> joltages{};
     joltages.reserve(lines.size());
+
+    // ========================= PART 1 ==================================0
 
     for (auto line : lines) {
         Arrow left(0, line[0]);
@@ -65,12 +103,48 @@ void solve() {
         if (middleNumber > leftNumber) highest = middleNumber;
         if (rightNumber > middleNumber) highest = rightNumber;
 
-        joltages.push_back(highest);
+        joltages.push_back(static_cast<unsigned long long>(highest));
     }
 
     int sum = std::accumulate(joltages.begin(), joltages.end(), 0);
 
     std::cout << "Day 03, part 1: " << sum << std::endl;
+
+    // ================================ PART 2 =========================================
+    joltages.clear();
+    joltages.reserve(lines.size());
+
+    for (auto line : lines) {
+        int numBatteries = 12;
+        std::vector<char> maxValues{};
+        unsigned long long maxJoltage = getFirstMax(line, numBatteries - 1, maxValues);
+
+        int index = line.size() - numBatteries;
+        for (index; index >= 0; index--) {
+            unsigned long long newMax = calculateSum(line[index], maxValues);
+
+            if (newMax <= maxJoltage) continue;
+
+            maxJoltage = newMax;
+            auto newMaxValues = maxValues;
+            char currentBattery = line[index];
+
+            for (int i = 0; i < maxValues.size(); i++) {
+                if (currentBattery < maxValues[i]) break; // Only update the maxValue (and all following it) if the current battery is equal or larger
+
+                newMaxValues[i] = currentBattery;
+                currentBattery = maxValues[i];
+            }
+
+            maxValues = newMaxValues;
+        }
+
+        joltages.push_back(maxJoltage);
+    }
+
+    unsigned long long sum2 = std::accumulate(joltages.begin(), joltages.end(), 0ULL);
+
+    std::cout << "Day 03, part 2: " << sum2 << std::endl;
 }
 
 }
